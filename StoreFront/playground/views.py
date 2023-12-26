a857b9f53aac19411c2f537f56b7bd7f3fc1e6d1
parinaz <p.sattarzadeh@gmail.com>
2023-12-26 22:46:04 +0330

    -Added dictionary to view.py -Added launch.json for debug and run(didnt work on my system!) - added if statement  to hrml

--- StoreFront/playground/views.py
+++ StoreFront/playground/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
-    return render(request,'hello.html')
+    x=1
+    y=2
+    return render(request,'hello.html',{'name': 'parinaz'})
     # return HttpResponse('Hello world')
